cfb3478ba8e81efa8a32bcab4b7aaa460ff2a096
Patilgrv <patilgaurav189@gmail.com>
2023-02-16 09:33:42 +0530

    assignment 01variableAssign
    completed 01variableAssign

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,14 @@
 console.log(`Gaurav Patil`);
+console.log("27");
+console.log('Indore');
+
+
+
+var fullName = 'Gaurav Patil';   //variable Declaration and Initialization in same line
+console.log(fullName);
+
+var city;      //variable Declaration
+city = 'pune'  // variable Initialization
+console.log(city);
+city = 'Indore'  // varible Update
+console.log(city);
