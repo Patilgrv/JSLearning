8eb2a6f3f492d088f0b4be277dd6b04000057452
Patilgrv <patilgaurav189@gmail.com>
2023-02-23 13:13:06 +0530

    Ternary operator
    Ternary operator

--- 05_operator.js
+++ 05_operator.js
@@ -26,3 +26,50 @@ let A= num + 20
 let B= num+=20
 
 console.log(A==B);
+
+
+console.log("marks >= 35"); //Greater thab equal
+// I mark greater than 35
+// Is mark greater than equal 35
+
+//Less than equal <=
+
+let marks = 70;
+let results = marks >=60 ?"Allow him for intervie":Dontallow; 
+console.log((result));
+
+var age = 20;
+var res = age>=21? true : false;
+console.log(res);
+
+
+
+
+console.log(" Check num is even or odd");
+let myNumber = 7;
+//Even- divide by  2 and reminder 0
+
+let Result = myNumber%2==0? "Even" : "Odd"
+console.log(`Given number is ${Result}`);
+
+var numA = 10; 
+var numB = 12;
+var resultA =  
+
+function wordLength(word){
+    var wordLength = word.length;
+    var result = wordLength%2==0 ? "EVEN" : "ODD";
+    return result;
+ }
+ var result = wordLength("JavaScript");
+ console.log(`Word "JavaScript" has ${result} length` );
+ // var result = wordLength("Google");
+ console.log(`Word "Google" has ${wordLength("Google Chrome")} length` );
+
+
+ function maleMarriageEligibility(gender, age, boyName){
+    var result = (gender=="Male" && age>=21) ? `Hey ${boyName} you are eligible for marriage` : " No Try next time" ;
+    return result;
+ }
+  var result = maleMarriageEligibility("Male", 25, "Billgates");
+  console.log(result);
--- dist/05_operator.dev.js
+++ dist/05_operator.dev.js
@@ -26,3 +26,30 @@ var num = 10; //compund addition +=
 var A = num + 20;
 var B = num += 20;
 console.log(A == B);
+console.log("marks >= 35"); //Greater thab equal
+// I mark greater than 35
+// Is mark greater than equal 35
+//Less than equal <=
+
+var marks = 70;
+var results = marks >= 60 ? "Allow him for intervie" : Dontallow;
+console.log(result);
+var age = 20;
+var res = age >= 21 ? true : false;
+console.log(res);
+console.log(" Check num is even or odd");
+var myNumber = 7; //Even- divide by  2 and reminder 0
+
+var Result = myNumber % 2 == 0 ? "Even" : "Odd";
+console.log("Given number is ".concat(Result));
+var numA = 10;
+var numB = 12;
+
+function wordlength(word) {
+  var wordlength = word.length;
+  var ressult = wordlength % 2 == 0 ? "Even" : "Odd";
+  return ressult;
+}
+
+var resultt = wordlength("JavaScript");
+console.log("Word \"Javascipt\" has ".concat(ressult, " length"));
