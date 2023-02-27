4553116041dd659911355f4ab6453caf5cbd2a9a
Patilgrv <patilgaurav189@gmail.com>
2023-02-27 21:39:58 +0530

    if ese

--- 06_if_block.js
+++ 06_if_block.js
@@ -1,27 +1,26 @@
 console.log("Start");
 var num = 10;
-if (num >0){
-    console.log(`inside if`);
-    console.log(`Number is Positive ${num}`);
+if (num > 0) {
+  console.log(`inside if`);
+  console.log(`Number is Positive ${num}`);
 }
 
 console.log(`End`);
 
 var ageForVote = 10;
 if (ageForVote >= 18) {
-    console.log(`You are eligible for voting`);
-    console.log(`Age is:${ageForVote}`);
-
+  console.log(`You are eligible for voting`);
+  console.log(`Age is:${ageForVote}`);
 }
 console.log(`End of next if block`);
 
-function checkEvenOdd(num){
-    if (num%2==0){
-        return "Even";
-    }
-    if (num%2!==0){
-        return "Odd"
-    }
+function checkEvenOdd(num) {
+  if (num % 2 == 0) {
+    return "Even";
+  }
+  if (num % 2 !== 0) {
+    return "Odd";
+  }
 }
 
 var result = checkEvenOdd(45);
@@ -31,22 +30,19 @@ var result = checkEvenOdd(70);
 console.log(`Given number 70 is ${result}`);
 
 var num1 = 5;
-if (num1>0){
-    console.log(`Number ${num1} is a positive`);
-}
-else{
-    console.log(`Number ${num1} is a Negative`);
-
+if (num1 > 0) {
+  console.log(`Number ${num1} is a positive`);
+} else {
+  console.log(`Number ${num1} is a Negative`);
 }
 
-function maleMarriageEligibility(gender, age,boyName){
-    if (gender=="Male" && age>21) {
-        console.log("You are eligible");
-    }
-    else{
-        console.log("Not Eligible");
-    }
+function maleMarriageEligibility(gender, age, boyName) {
+  if (gender == "Male" && age > 21) {
+    console.log("You are eligible");
+  } else {
+    console.log("Not Eligible");
+  }
 }
 
-maleMarriageEligibility("Male",25,"Billgates");
-maleMarriageEligibility("Male",20,"Anil");
+maleMarriageEligibility("Male", 25, "Billgates");
+maleMarriageEligibility("Male", 20, "Anil");
