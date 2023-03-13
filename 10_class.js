9d02211f90dba82ba1da2dcc371a5363f805f44f
Patilgrv <patilgaurav189@gmail.com>
2023-03-14 01:53:24 +0530

    constructor
    constructor

--- 10_class.js
+++ 10_class.js
@@ -13,3 +13,17 @@ console.log(sbiBank);
 
 const axisBank = new Bank("Axis Bank", "Bajirao Road, Pune", "4597863258", "AXIS4567893", 10.50);
 console.log(axisBank);
+
+
+class Person {
+    constructor(fullName, city){
+        this.fullName = fullName;
+        this.city = city;
+    }
+}
+const personDhoni = new Person("MS Dhoni", "Ranchi");
+const personShubham = new Person("Shubham Gill", "Mumbai");
+
+console.log(personDhoni instanceof Person);
+console.log(personShubham instanceof Bank);
+console.log(sbiBank instanceof Bank);
--- dist/10_class.dev.js
+++ dist/10_class.dev.js
@@ -16,3 +16,16 @@ var sbiBank = new Bank("SBI Bank", "Wakad", "62514897564", "SBIN0004598", 10.75)
 console.log(sbiBank);
 var axisBank = new Bank("Axis Bank", "Bajirao Road, Pune", "4597863258", "AXIS4567893", 10.50);
 console.log(axisBank);
+
+var Person = function Person(fullName, city) {
+  _classCallCheck(this, Person);
+
+  this.fullName = fullName;
+  this.city = city;
+};
+
+var personDhoni = new Person("MS Dhoni", "Ranchi");
+var personShubham = new Person("Shubham Gill", "Mumbai");
+console.log(personDhoni instanceof Person);
+console.log(personShubham instanceof Bank);
+console.log(sbiBank instanceof Bank);
